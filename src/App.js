2394b292d329129f03c3e2c32a8bc28ee32ec9e4
jatindarkhatry <khatrij784@gmail.com>
2022-10-05 22:56:38 +0500

    firbase hosting added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cv from "./components/cv";
+
+function App() {
+  return (
+    <div className="App">
+      <Cv />
+    </div>
+  );
+}
+
+export default App;
